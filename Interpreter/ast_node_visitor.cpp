d691fd56d0ff60c908c2ea473ea5b15acd53a639
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2019-11-21 21:41:19 +0100

    Default node traversal added
    This should it easier to add new visitors.

--- Interpreter/ast_node_visitor.cpp
+++ Interpreter/ast_node_visitor.cpp
@@ -5,14 +5,18 @@ void ast_node_visitor::visit(ast_node& ast_node)
 	ast_node.accept(*this);
 }
 
-void ast_node_visitor::visit(bin_op&)
+void ast_node_visitor::visit(bin_op& node)
 {
-	// For derived classes to implement
+	// For derived classes to implement. By default, traversed into the node.
+	
+	node.left()->accept(*this);
+	node.right()->accept(*this);
 }
 
-void ast_node_visitor::visit(unary_op&)
+void ast_node_visitor::visit(unary_op& node)
 {
-	// For derived classes to implement
+	// For derived classes to implement. By default, traversed into the node.
+	node.expr()->accept(*this);
 }
 
 void ast_node_visitor::visit(num&)
