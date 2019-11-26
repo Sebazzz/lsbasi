f97fc4cc78b554eb62a79d17c52df37fbb0ec505
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2019-11-26 19:25:38 +0100

    Properly implement case-insensitive search

--- Interpreter/util.h
+++ Interpreter/util.h
@@ -8,18 +8,14 @@
  */
 struct case_insensitive_string_comparer
 {
-	bool operator()(const std::wstring& s1, const std::wstring& s2) const
-	{
-		// This implementation works, but is suboptimal because it allocates
-		// a lot for a simple comparison. TODO: find a better way
-		
-		std::wstring str1(s1.length(), ' ');
-		std::wstring str2(s2.length(), ' ');
-
-		const auto myLambda = [](wchar_t c) -> wchar_t { return towlower(c); };
-		std::transform(s1.begin(), s1.end(), str1.begin(), myLambda);
-		std::transform(s2.begin(), s2.end(), str2.begin(), myLambda);
-
-		return str1 < str2;
-	}
+    struct compare_internal
+    {
+      bool operator() (const wchar_t& c1, const wchar_t& c2) const {
+          return std::tolower (c1) < std::tolower (c2); 
+      }
+    };
+	
+    bool operator() (const std::wstring& s1, const std::wstring& s2) const {
+        return std::lexicographical_compare (s1.begin (), s1.end (), s2.begin (), s2.end (), compare_internal());
+    }
 };
