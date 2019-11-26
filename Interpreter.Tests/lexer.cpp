c0cde0a4edeb1e36edcf4e479b46c7282b69a3d2
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2019-11-26 21:16:00 +0100

    lexer: add tests for idiv vs real div

--- Interpreter.Tests/lexer.cpp
+++ Interpreter.Tests/lexer.cpp
@@ -31,6 +31,34 @@ TEST_CASE( "Lexer tokenize test - single line + real", "[lexer]" ) {
     REQUIRE( do_lex(lex) == std::wstring(L"TOK(.)") );
 }
 
+TEST_CASE( "Lexer tokenize test - single line - division int", "[lexer]" ) {
+    lexer lex(L"BEGIN a := 2 div 4; END.");
+
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(begin,BEGIN)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(idf,a)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(:=)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(int,2)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(idiv, DIV)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(int,4)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(semi)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(end,END)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(.)") );
+}
+
+TEST_CASE( "Lexer tokenize test - single line - division real", "[lexer]" ) {
+    lexer lex(L"BEGIN a := 2 / 4; END.");
+
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(begin,BEGIN)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(idf,a)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(:=)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(int,2)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(rdiv)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(int,4)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(semi)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(end,END)") );
+    REQUIRE( do_lex(lex) == std::wstring(L"TOK(.)") );
+}
+
 TEST_CASE( "Lexer tokenize test - multiline", "[lexer]" ) {
     lexer lex(L"\
 BEGIN\
