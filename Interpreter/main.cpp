9756a4d59a7cf6bdddbca3f2a865abaf3bf1cf20
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2019-11-27 20:40:07 +0100

    Allow specifying file path on command line

--- Interpreter/main.cpp
+++ Interpreter/main.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include "interpreter.h"
 #include "interpret_except.h"
 #include "console_color.h"
@@ -19,7 +20,7 @@ std::wstring make_lisp_notation(std::wstring& input)
     return invoke_repl_visitor<lisp_notation_visitor>(input);
 }
 
-int main()
+void repl_mode()
 {
 	std::wcout << L"Enter your expressions" << std::endl;
 
@@ -58,4 +59,62 @@ int main()
 		
 		std::wcout << std::endl;
 	}
+}
+
+int interpret_file(const std::wstring& file_path)
+{
+	std::wcout << "Interpreting: " << file_path << std::endl;
+	
+	std::wifstream file(file_path);
+
+	if (file.bad())
+	{
+		std::wcout << "Unable to open file." << std::endl;
+		return -1;
+	}
+	
+	std::wstring all;
+	std::wstring buf;
+	while(std::getline(file, buf)) {
+	     all += buf;
+	}
+
+	interpreter interpreter(all);
+	try
+	{
+		std::wcout << interpreter.interpret() << std::endl;
+	} catch (interpret_except& e)
+	{
+		std::wcout << "Error interpreting [" << file_path << "]" << std::endl;
+		std::wcout << e.what() << std::endl;
+
+		return -1;
+	}
+
+	std::wcout << "Done." << std::endl;
+
+	return 0;
+}
+
+int wmain(int argc, wchar_t* argv[])
+{
+	if (argc <= 1)
+	{
+		std::wcout << "No arguments were given. Welcome to REPL mode" << std::endl << std::endl;
+
+		repl_mode();
+
+		return 0;
+	}
+
+	const std::wstring first_arg = argv[1];
+	if (first_arg == L"--help")
+	{
+		std::wcout << "SD Pascal Interpreter" << std::endl;
+		std::wcout << "Usage: Interpreter.exe [file]" << std::endl;
+		std::wcout << std::endl;
+		return 0;
+	}
+
+	return interpret_file(first_arg);
 }
