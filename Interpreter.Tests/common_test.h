3019642083f34750a9477ca9ed15b0f2255a67e0
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2019-12-09 19:51:36 +0100

    Move the test support code for the intepreter to its own file

--- Interpreter.Tests/common_test.h
+++ Interpreter.Tests/common_test.h
@@ -10,3 +10,4 @@
 
 // Support files for test
 #include "string_conv_util.h"
+#include "interpreter_test_support.h"
---
+++ Interpreter.Tests/interpreter_test_support.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "../Interpreter/interpreter.h"
+#include "../Interpreter/symbol.h"
+#include "../Interpreter/type.h"
+#include "../Interpreter/routine_symbol.h"
+#include "../Interpreter/var_decl.h"
+#include "../Interpreter/builtin_type_symbol.h"
+
+struct interpret_result
+{
+    std::shared_ptr<scope_context> global_scope;
+    interpreter_context_ptr context;
+    ast_ptr ast;
+    std::wstring output;
+};
+
+builtin_integer verify_int_symbol(const interpret_result& result, const symbol_identifier& identifier);
+builtin_real verify_real_symbol(const interpret_result& result, const symbol_identifier& identifier);
+builtin_string verify_string_symbol(const interpret_result& result, const symbol_identifier& identifier);
+interpret_result do_interpret_program(const char* input);
